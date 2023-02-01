6503741e62545a3903beaf87442be4b9491c0af7
Gurnoor Singh Saini <92860451+Sainigurnoor511@users.noreply.github.com>
2023-02-02 00:37:50 +0530

    Update Password verification .py

--- Password verification .py
+++ Password verification .py
@@ -1,15 +1,17 @@
+/*program to implement Hashing Using SHA-256
+Author : Gurnoor Singh Saini */
+
 import hashlib
 import secrets
 import string
 
-
 N = 7
 res = ''.join(secrets.choice(string.ascii_uppercase + string.digits)
               for i in range(N))
-
-
+// Sign up Block
 print ("\n\t SIGN UP")
 
+//Sign-up Class
 class signup : 
     " Class for Sign-up "
 
@@ -21,9 +23,10 @@ class signup :
     result = hashlib.sha256(saltedpassword.encode())
     hash = result.hexdigest()
 
-
+//Login Block
 print ("\n\t LOGIN")
 
+//Login Class
 class login :
     " Class for Login "
 
